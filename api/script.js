bad2148dfd940613fac02c27df279911eada6aec
Brian Cordoba <briancormin@gmail.com>
2023-12-01 16:32:44 -0500

    Se actualizó la pokeApi

--- api/script.js
+++ api/script.js
@@ -2,11 +2,35 @@
 document.addEventListener('DOMContentLoaded', pokeApi);
 
 /* Accediendo a los pokemon*/
-const URL = 'https://pokeapi.co/api/v2/pokemon/';
+let URL = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20';
+let contador = 0;
 const spinner = document.getElementById('spinner');
 const errorMsg = document.getElementById('errorMsg');
 const pokemonName = document.getElementById('pokemonName');
 const pokeCards = document.getElementById('pokeCards');
+const listado = document.getElementById('listado');
+
+
+/* NEXT 20 */
+const nextBtn = document.getElementById('nextBtn');
+nextBtn.addEventListener('click', ()=>{
+  contador += 20;
+  URL = `https://pokeapi.co/api/v2/pokemon?offset=${contador}&limit=20`
+  pokeApi();
+  disable(contador);
+  indiceListado(contador);
+});
+
+/* PREVIOUS 20 */
+const previousBtn = document.getElementById('previousBtn');
+previousBtn.addEventListener('click', ()=>{
+  contador -= 20;
+  URL = `https://pokeapi.co/api/v2/pokemon?offset=${contador}&limit=20`
+  pokeApi();
+  disable(contador);
+  indiceListado(contador);
+});
+
 
 
 async function pokeApi(){
@@ -20,6 +44,8 @@ async function pokeApi(){
     spinner.classList.toggle('hide'); 
   }
 }
+
+
 
 async function mostrarNombres(pokemons) {
   const pokeCards = document.getElementById('pokeCards')
@@ -35,14 +61,29 @@ async function mostrarNombres(pokemons) {
   pokeCards.innerHTML = contenidoHTML;
 }
 
-
 async function pokeImagen(pokeURL){
   try{
     let resultado = await fetch(pokeURL);
     return await resultado.json();
   } catch (error){
     console.log(error)
-  }
+  } 
 
 }
 
+
+/* Disable btns */
+function disable(contador){
+  if(contador === 0){
+    previousBtn.setAttribute('disabled', '');
+  } else {
+    previousBtn.removeAttribute('disabled');
+  }
+}
+disable(contador);
+
+/* Indice del listado */
+function indiceListado(contador){
+  listado.textContent = `Pokemons del ${contador + 1} al ${contador + 20}`;
+}
+indiceListado(contador);
